2e8364f0c7c1751bcfa31c8aa02e4154335935a8
GeorgeBennett <GBennettOfficial@outlook.com>
2025-06-29 14:36:40 -0400

    deploy

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-    baseURL: "http://localhost:5001/api/",
+    baseURL: "https://chatty-api-m3oi.onrender.com/api/",
     withCredentials: true,
 })
